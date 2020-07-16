ca1c0fa823cfc163c7e3a0548aae16276ad5b888
whichxjy <whichxjy@gmail.com>
2020-07-16 15:35:49 +0800

    Scale video

--- js/sketch.js
+++ js/sketch.js
@@ -25,16 +25,19 @@ function handlePoses(poses) {
 
 function draw() {
   push();
-  translate(video.width, 0);
+  translate(windowWidth, 0);
   scale(-1, 1);
-  image(video, 0, 0);
+  image(video, 0, 0, windowWidth, windowHeight);
   pop();
+
+  const widthScale = windowWidth / video.width;
+  const heightScale = windowHeight / video.height;
 
   if (pose) {
     fill(255, 0, 0);
-    ellipse(pose.nose.x, pose.nose.y, 32);
+    ellipse(pose.nose.x * widthScale, pose.nose.y * heightScale, 32);
     fill(0, 0, 255);
-    ellipse(pose.leftWrist.x, pose.leftWrist.y, 32);
-    ellipse(pose.rightWrist.x, pose.rightWrist.y, 32);
+    ellipse(pose.leftWrist.x * widthScale, pose.leftWrist.y * heightScale, 32);
+    ellipse(pose.rightWrist.x * widthScale, pose.rightWrist.y * heightScale, 32);
   }
 }
