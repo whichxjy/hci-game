945c42e004de73dd3c18c8cfc539b75c57a363a4
whichxjy <whichxjy@gmail.com>
2020-07-16 15:25:03 +0800

    Import ml5

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,40 @@
 let video;
+let poseNet;
+let pose;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
   video = createCapture(VIDEO);
   video.hide();
+
+  console.log(video.height);
+
+  poseNet = ml5.poseNet(video, { flipHorizontal: true, detectionType: "single" }, () => {
+    console.log("poseNet ready");
+  });
+
+  poseNet.on("pose", handlePoses);
+}
+
+function handlePoses(poses) {
+  if (poses.length > 0) {
+    pose = poses[0].pose;
+  }
 }
 
 function draw() {
+  push();
+  translate(video.width, 0);
+  scale(-1, 1);
   image(video, 0, 0);
+  pop();
+
+  if (pose) {
+    fill(255, 0, 0);
+    ellipse(pose.nose.x, pose.nose.y, 32);
+    fill(0, 0, 255);
+    ellipse(pose.leftWrist.x, pose.leftWrist.y, 32);
+    ellipse(pose.rightWrist.x, pose.rightWrist.y, 32);
+  }
 }
