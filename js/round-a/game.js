6443b454e7f23cefe28522e83468e917d4eb7c46
whichxjy <whichxjy@gmail.com>
2020-07-17 12:18:52 +0800

    Add game level

--- js/round-a/game.js
+++ js/round-a/game.js
@@ -7,6 +7,8 @@ function Game() {
   let widthScale;
   let heightScale;
 
+  let monsterNum = 2;
+
   const poseNet = ml5.poseNet(video, { flipHorizontal: true, detectionType: "single" }, () => {
     console.log("poseNet ready");
 
@@ -22,21 +24,28 @@ function Game() {
 
   const monsters = [];
 
-  for (let i = 0; i < 5; i++) {
-    const position = createVector(random(windowWidth), random(windowHeight));
-    const velocity = createVector(0, 0);
-    const size = 40;
-    const maxForce = random(5, 10);
-    const maxSpeed = random(5, 10);
-    monsters.push(new Monster(position, velocity, size, maxForce, maxSpeed));
-  }
+  this.getSystem = () => {
+    monsters.length = 0;
 
-  const resolution = 20;
-  const system = new FlowFieldSystem(monsters, resolution);
+    for (let i = 0; i < monsterNum; i++) {
+      const position = createVector(random(windowWidth), random(windowHeight));
+      const velocity = createVector(0, 0);
+      const size = 40;
+      const maxForce = random(5, 10);
+      const maxSpeed = random(5, 10);
+      monsters.push(new Monster(position, velocity, size, maxForce, maxSpeed));
+    }
+
+    const resolution = 20;
+
+    return new FlowFieldSystem(monsters, resolution);
+  };
+
+  let system = this.getSystem();
 
   setInterval(() => {
     system.reset();
-  }, 1500);
+  }, 3000);
 
   this.draw = () => {
     if (showVideo) {
@@ -58,10 +67,23 @@ function Game() {
       leftController.display();
       rightController.display();
 
+      let allDead = true;
+
       for (let i = 0; i < monsters.length; i++) {
+        if (monsters[i].dead) {
+          continue;
+        }
+
+        allDead = false;
+
         if (leftController.hit(monsters[i]) || rightController.hit(monsters[i])) {
           monsters[i].die();
         }
+      }
+
+      if (allDead) {
+        monsterNum += 1;
+        system = this.getSystem();
       }
     }
   };
