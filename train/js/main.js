c183d197a3e761b3849da525d8fe692d4839cb78
whichxjy <whichxjy@gmail.com>
2020-07-17 20:38:23 +0800

    Add train method

--- train/js/main.js
+++ train/js/main.js
@@ -6,6 +6,15 @@ let heightScale;
 let pose;
 let skeleton;
 
+let brain;
+
+const STATE_WAITING = 1;
+const STATE_COLLECTING = 2;
+
+let state = STATE_WAITING;
+
+let targetLabel;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
@@ -23,8 +32,52 @@ function setup() {
     if (poses.length > 0) {
       pose = poses[0].pose;
       skeleton = poses[0].skeleton;
+
+      if (state === STATE_COLLECTING) {
+        let inputs = [];
+
+        for (let i = 0; i < pose.keypoints.length; i++) {
+          let x = pose.keypoints[i].position.x;
+          let y = pose.keypoints[i].position.y;
+          inputs.push(x);
+          inputs.push(y);
+        }
+
+        let target = [targetLabel];
+        brain.addData(inputs, target);
+      }
     }
   });
+
+  brain = ml5.neuralNetwork({
+    inputs: 34,
+    outputs: 4,
+    task: "classification",
+    debug: true
+  });
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    brain.saveData();
+  } else if (keyCode === 13) {
+    brain.normalizeData();
+    brain.train({ epochs: 50 }, () => {
+      console.log("Model trained");
+      brain.save();
+    });
+  } else if (state === STATE_WAITING) {
+    setTimeout(() => {
+      targetLabel = key;
+      console.log("Collecting: " + key);
+      state = STATE_COLLECTING;
+
+      setTimeout(() => {
+        console.log("Done");
+        state = STATE_WAITING;
+      }, 10000);
+    }, 2000);
+  }
 }
 
 function draw() {
